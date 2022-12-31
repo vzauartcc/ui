e0a7c8a384a5324f9dae7271edb78e2a42510712
MCampbell <cmvzjx@gmail.com>
2022-12-31 10:28:37 -0600

    404

--- src/views/page/404.vue
+++ src/views/page/404.vue
@@ -6,7 +6,7 @@
 				OPE! Matt made a problem dont ya know well tell your folks I says hi!.
 			</p>
 			<p>
-				We're sorry, but the content you request could not be found. If you believe that you are receiving this message in error, please email the webmaster. In the meantime, I have provided a 747 to ease your sorrows.
+				We're sorry, but the content you request could not be found. If you believe that you are receiving this message in error, please email the webmaster. In the meantime, I have provided a F14 to ease your sorrows.
 			</p>
 			<p>
 				<img src="https://cdn.discordapp.com/attachments/1058781321381560421/1058781413626871808/f14_sorrow.webp">
