da3079e8c826c9d710f01ebb60bb3ea0dbf9411b
MCampbell <cmvzjx@gmail.com>
2022-12-31 10:20:40 -0600

    Error 404
    404 Page Update

--- src/views/page/404.vue
+++ src/views/page/404.vue
@@ -3,13 +3,13 @@
 		<div class="card-content">
 			<span class="card-title">404 – Page Not Found</span>
 			<p>
-				Daan made an uh-oh.
+				OPE! Matt made a problem dont ya know well tell your folks I says hi!.
 			</p>
 			<p>
-				We're sorry, but the content you request could not be found. If you believe that you are receiving this message in error, please email the webmaster. In the meantime, a cute pupper has been dispatched to ease your sorrows.
+				We're sorry, but the content you request could not be found. If you believe that you are receiving this message in error, please email the webmaster. In the meantime, I have provided a 747 to ease your sorrows.
 			</p>
 			<p>
-				<img src="@/assets/images/dog.jpg" alt="Cute dog">
+				<img src="https://cdn.discordapp.com/attachments/1058781321381560421/1058781413626871808/f14_sorrow.webp">
 			</p>
 		</div>
 	</div>
