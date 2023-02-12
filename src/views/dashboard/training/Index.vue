0db65fc9e8b42d19e504228c661c0cdaa88ad94a
MCampbell <cmvzjx@gmail.com>
2023-02-12 17:49:23 -0600

    Dashboard Index
    Code Functional Now Allows Students to Cancel Training Sessions still working on add ability to Edit Sessions

--- src/views/dashboard/training/Index.vue
+++ src/views/dashboard/training/Index.vue
@@ -9,8 +9,10 @@
 		<div class="loading_container" v-if="!upcomingSessions">
 			<Spinner />
 		</div>
+
 		<p class="no_sessions" v-else-if="upcomingSessions && upcomingSessions.length === 0">You have no upcoming training sessions.</p>
-		<div class="session_wrapper" v-else>
+
+		<div class="session_wrapper">
 			<table class="session_list striped">
 				<thead class="session_list_head">
 					<tr>
@@ -18,6 +20,7 @@
 						<th>Start Time</th>
 						<th>End Time</th>
 						<th>Instructor</th>
+						<th class='options'>Options</th>
 					</tr>
 				</thead>
 				<tbody class="session_list_row" v-if="upcomingSessions">
@@ -26,6 +29,22 @@
 						<td>{{dtLong(session.startTime)}}</td>
 						<td>{{dtLong(session.endTime)}}</td>
 						<td>{{session.instructor ? (session.instructor.fname + ' ' + session.instructor.lname) : 'Unfulfilled'}}</td>
+						<td class="options">
+                            <a :href="`#modal_delete_${session._id}`" data-position="top" data-tooltip="Cancel Training Session"
+							class="tooltipped modal-trigger"><i class="material-icons red-text text-darken-2">cancel</i></a>
+                        </td>
+                        <div :id="`modal_delete_${session._id}`" class="modal modal_delete">
+                            <div class="modal-content">
+                                <h4>Cancel Training Session?</h4>
+                                <p>This will remove the training session.  <br>
+								<br>If you are cancelling a scheduled session within 24 hours, please advise the instructor or ta@zauartcc.net via email also.<br><br>
+								Are you sure you wish to cancel this session?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="#!" @click="deleteSession(session._id)" class="btn waves-effect">Delete</a>
+                                <a href="#!" class="btn-flat waves-effect modal-close">Cancel</a>
+                            </div>
+                        </div>
 					</tr>
 				</tbody>
 			</table>
@@ -51,11 +70,34 @@ export default {
 	},
 	async mounted() {
 		await this.getUpcomingSessions();
-	},
+
+		M.Tooltip.init(document.querySelectorAll('.tooltipped'), {
+   			margin: 0
+			}) ;
+		M.Modal.init(document.querySelectorAll('.modal'), {
+		preventScrolling: false
+			}) ;
+		},
+
 	methods: {
 		async getUpcomingSessions() {
 			const {data} = await zabApi.get(`/training/request/upcoming`);
 			this.upcomingSessions = data.data;
+		},
+
+		async deleteSession(id)
+		{
+			console.log(id);
+			const {data} = await zabApi.delete(`/training/request/${id}`);
+
+			this.$nextTick(() => {
+                        M.Modal.getInstance(document.querySelector('.modal_delete')).close();
+
+			this.upcomingSessions = [];
+			this.getUpcomingSessions();
+
+			document.location.reload();
+			});
 		}
 	}
 
