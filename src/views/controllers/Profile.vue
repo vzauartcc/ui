a891eafe966d34777e319995f963b36d0944923c
Tim Barker <76476900+SGBM-Jimbo@users.noreply.github.com>
2023-02-24 17:42:38 -0600

    Update Profile.vue (#52)
    * Update Profile.vue
    
    update Controller Profile to  show reduced certs
    
    * Update Index.vue
    
    updated approvedAirports on controller dashboard

--- src/views/controllers/Profile.vue
+++ src/views/controllers/Profile.vue
@@ -136,7 +136,7 @@ export default {
 					certsToShow.push(cert);
 				} else {
 					const certPos = cert.code.slice(-3);
-					if(!hasCerts.includes(`p50${certPos}`)) {
+					if(!hasCerts.includes(`ord${certPos}`)) {
 						certsToShow.push(cert);
 					}
 				}
