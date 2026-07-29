8ee4b15e495eb0d811a4b4e2176f24227f488d1d
Ryan Savara <32147285+ryansavara@users.noreply.github.com>
2026-07-29 09:53:30 -0400

    fix footer layout to improve mobile experience. Closes #228

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -38,47 +38,54 @@ onUnmounted(() => {
 <template>
   <footer class="bg-bg-5 text-white">
     <div class="md:w-10/12 mx-auto text-center">
-      <div class="my-5">
+      <div class="m-5">
         <p>
           This site is not affiliated with the FAA, or any governing aviation
           body. All content contained herein is approved only for use on the
           VATSIM network.
         </p>
       </div>
-      <div class="flex justify-between mx-auto p-1">
-        <router-link to="/">
-          <img
-            class="mainLogo"
-            :src="`${s3Url}/site-logo/Main-Logo.png`"
-            draggable="false"
-            style="height: 4rem"
-            alt="Virtual Chicago ARTCC Logo" />
-        </router-link>
-        <a href="https://vatusa.net">
-          <img
-            :src="`${s3Url}/site-logo/vatusa.svg`"
-            :draggable="false"
-            style="height: 4rem"
-            alt="VATUSA Subdivision Logo" />
-        </a>
-        <a href="https://vatsim.net">
-          <img
-            :src="`${s3Url}/site-logo/vatsim.png`"
-            :draggable="false"
-            style="height: 4rem"
-            alt="VATSIM Network Logo" />
-        </a>
+      <div class="flex justify-center items-center">
+        <div class="grid md:grid-cols-3 gap-4">
+          <router-link to="/" class="flex justify-center">
+            <img
+              class="h-16 object-contain"
+              :src="`${s3Url}/site-logo/Main-Logo.png`"
+              draggable="false"
+              alt="Virtual Chicago ARTCC Logo" />
+          </router-link>
+          <a href="https://vatusa.net" class="flex justify-center">
+            <img
+              class="h-16 object-contain"
+              :src="`${s3Url}/site-logo/vatusa.svg`"
+              :draggable="false"
+              alt="VATUSA Subdivision Logo" />
+          </a>
+          <a href="https://vatsim.net" class="flex justify-center">
+            <img
+              class="h-16 object-contain"
+              :src="`${s3Url}/site-logo/vatsim.png`"
+              :draggable="false"
+              alt="VATSIM Network Logo" />
+          </a>
+        </div>
       </div>
-      <div class="flex flex-row gap-2 justify-center my-5 text-sm">
+
+      <div
+        class="flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-3 my-5 text-sm text-center">
         <div class="">
           Copyright &copy; {{ new Date().getFullYear() }} Virtual Chicago ARTCC
         </div>
-        <div>•</div>
+
+        <div class="hidden sm:block">•</div>
+
         <div class="font-bold">
           <router-link to="/privacy-policy"> Privacy Policy </router-link>
         </div>
-        <div>•</div>
-        <div class="inline-flex align-top gap-1">
+
+        <div class="hidden sm:block">•</div>
+
+        <div class="inline-flex items-center gap-1">
           Open source on
           <a
             href="https://github.com/vzauartcc"
@@ -89,11 +96,13 @@ onUnmounted(() => {
             <span>GitHub</span>
           </a>
         </div>
-        <div>•</div>
-        <div class="inline-flex align-top">{{ zuluTime }}</div>
+
+        <div class="hidden sm:block">•</div>
+
+        <div class="inline-flex items-center">{{ zuluTime }}</div>
       </div>
     </div>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="css"></style>
